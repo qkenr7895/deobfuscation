76fd53bd34719d7ac34e25e88f969a848de45c1c
qkenr7895 <qkenr7895@naver.com>
2024-11-07 04:04:48 -0800

    push

--- test/test.c
+++ test/test.c
@@ -2,39 +2,27 @@
 #include <stdlib.h>
 
 void func2() {
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
+    __asm__ __volatile__ ("movaps %xmm3, %xmm3\n");
+    __asm__ __volatile__ ("movaps %xmm3, %xmm3\n");
+    __asm__ __volatile__ ("movaps %xmm3, %xmm3\n");
 }
 
 void func() {
 
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
-    __asm__ __volatile__ ("pxor %xmm2, %xmm2\n");
+    __asm__ __volatile__ ("movaps %xmm1, %xmm1\n");
+    __asm__ __volatile__ ("movaps %xmm1, %xmm1\n");
+    __asm__ __volatile__ ("movaps %xmm1, %xmm1\n");
 
     func2();
 }
 
 int main() {
     printf("before func\n");
-    __asm__ __volatile__ ("pxor %xmm0, %xmm0\n");
+    __asm__ __volatile__ ("movaps %xmm0, %xmm0\n");
     // VM Enter
     func();
     // VM Exit
-    __asm__ __volatile__ ("pxor %xmm1, %xmm1\n");
+   __asm__ __volatile__ ("movaps %xmm2, %xmm2\n");
     printf("after func\n");
 }
 
